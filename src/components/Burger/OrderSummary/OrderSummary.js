960d3a9972ef0c3c85e03da56debe748e3a96c5c
Suraj Gawas <suraj.gawas@nike.com>
2021-01-23 17:56:09 -0800

    performance optimization

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    
-    const ingredientsSummary = Object.keys(props.ingredients)
-        .map(igItem => {
-            return (
-                props.ingredients[igItem] > 0 ?
-                    <li key={igItem}>
-                        <span style={{textTransform: 'capitalize'}}>{igItem}</span>: {props.ingredients[igItem]}
-                    </li>
-                    : null
-                
-            )
-        })
+class OrderSummary extends Component {
 
-    return(
-        <Aux>
-            <h2>Order Details</h2>
-            <h4>Your Order Total: ${props.orderTotal.toFixed(2)}</h4>
-            <p>A spicy delicious burger with following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p>Click Continue to checkout.</p>
-            <Button btnType="Success" clicked={props.continueOrderSummary}>Continue</Button>
-            <Button btnType="Danger" clicked={props.cancelOrderSummary}>Cancel</Button>
-        </Aux>
-    )
+    componentDidUpdate(){
+        console.log('[OrderSummary] did update')
+    }
+    render(){
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+            .map(igItem => {
+                return (
+                    this.props.ingredients[igItem] > 0 ?
+                        <li key={igItem}>
+                            <span style={{textTransform: 'capitalize'}}>{igItem}</span>: {this.props.ingredients[igItem]}
+                        </li>
+                        : null
+                    
+                )
+            })
+
+        return(
+            <Aux>
+                <h2>Order Details</h2>
+                <h4>Your Order Total: ${this.props.orderTotal.toFixed(2)}</h4>
+                <p>A spicy delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p>Click Continue to checkout.</p>
+                <Button btnType="Success" clicked={this.props.continueOrderSummary}>Continue</Button>
+                <Button btnType="Danger" clicked={this.props.cancelOrderSummary}>Cancel</Button>
+            </Aux>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop clicked={props.cancelOrderSummary} showSummary={props.showOrderSummary}/>
-        <div className={classes.Modal}
-            style={{
-                transform: props.showOrderSummary ? 'translateY(0)': 'translateY(-100vh)',
-                opacity: props.showOrderSummary ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component{
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.showOrderSummary !== this.props.showOrderSummary;
+    }
 
-export default modal;
+    componentDidUpdate(){
+        console.log('[Modal] did update')
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Backdrop 
+                    clicked={this.props.cancelOrderSummary} 
+                    showSummary={this.props.showOrderSummary}/>
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.showOrderSummary ? 'translateY(0)': 'translateY(-100vh)',
+                        opacity: this.props.showOrderSummary ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+} 
+
+export default Modal;
